2998d62e9719751ae2fb38f640443ea8f6ee7688
menghonglin <mhlplus@163.com>
2019-09-03 20:28:20 +0800

    #comment 重构

--- src/main/java/com/com/mhl/Hiker.java
+++ src/main/java/com/com/mhl/Hiker.java
@@ -2,6 +2,8 @@ package com.com.mhl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Hiker {
     public static List<String> getAnagrams(final String word) {
@@ -12,12 +14,22 @@ public class Hiker {
             return anagrams;
         }
 
-        for (int i = 0; i < word.length(); i++) {
-            final List<String> innerAnagrams = getAnagrams(word.replace(word.charAt(i) + "", ""));
-            for (String innerAnagram : innerAnagrams) {
-                anagrams.add(word.charAt(i) + innerAnagram);
-            }
-        }
-        return anagrams;
+
+        return  IntStream.range(0, word.length())
+                .boxed()
+                .flatMap(i -> {
+                    final List<String> anagrams1 = getAnagrams(word.replace(word.charAt(i) + "", ""));
+                    return anagrams1.stream().map(anagram -> word.charAt(i) + anagram);
+                }).collect(Collectors.toList());
+
+//        System.out.println(collect.size());
+//
+//        for (int i = 0; i < word.length(); i++) {
+//            final List<String> innerAnagrams = getAnagrams(word.replace(word.charAt(i) + "", ""));
+//            for (String innerAnagram : innerAnagrams) {
+//                anagrams.add(word.charAt(i) + innerAnagram);
+//            }
+//        }
+//        return anagrams;
     }
 }
