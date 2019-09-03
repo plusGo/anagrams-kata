16479329c0cbb09710e3c46201cd7524c89012ab
menghonglin <mhlplus@163.com>
2019-09-03 20:14:36 +0800

    #comment tdd

--- src/main/java/com/com/mhl/Hiker.java
+++ src/main/java/com/com/mhl/Hiker.java
@@ -1,4 +1,23 @@
 package com.com.mhl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hiker {
+    public static List<String> getAnagrams(final String word) {
+        final List<String> anagrams = new ArrayList<>();
+
+        if (word.length() == 1) {
+            anagrams.add(word);
+            return anagrams;
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            final List<String> innerAnagrams = getAnagrams(word.replace(word.charAt(i) + "", ""));
+            for (String innerAnagram : innerAnagrams) {
+                anagrams.add(word.charAt(i) + innerAnagram);
+            }
+        }
+        return anagrams;
+    }
 }
--- src/test/java/com/mhl/HikerTest.java
+++ src/test/java/com/mhl/HikerTest.java
@@ -1,14 +1,59 @@
 package com.mhl;
 
+import com.com.mhl.Hiker;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class HikerTest {
     @Test
     public void should_get_1_when_input_b() {
         // given
+        final String word = "b";
 
         // when
+        final List<String> anagrams = Hiker.getAnagrams(word);
 
         // then
+        assertEquals(1, anagrams.size());
+    }
+
+    @Test
+    public void should_get_2_when_input_bi() {
+        // given
+        final String word = "bi";
+
+        // when
+        final List<String> anagrams = Hiker.getAnagrams(word);
+
+        // then
+        assertEquals(2, anagrams.size());
+    }
+
+    @Test
+    public void should_get_6_when_input_bi() {
+        // given
+        final String word = "bir";
+
+        // when
+        final List<String> anagrams = Hiker.getAnagrams(word);
+
+        // then
+        assertEquals(6, anagrams.size());
+    }
+
+    @Test
+    public void should_get_24_when_input_biro() {
+        // given
+        final String word = "biro";
+
+        // when
+        final List<String> anagrams = Hiker.getAnagrams(word);
+
+        // then
+        System.out.println(anagrams);
+        assertEquals(24, anagrams.size());
     }
 }
